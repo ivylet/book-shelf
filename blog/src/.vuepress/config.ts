d2f53212fce52911c295cb4f307e1bfc8313c86c
saytaytay <93778340+saytaytay@users.noreply.github.com>
2022-11-28 21:04:36 +0800

    deploy

--- blog/src/.vuepress/config.ts
+++ blog/src/.vuepress/config.ts
@@ -1,17 +1,20 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
+import { searchPlugin } from "@vuepress/plugin-search";
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
-// import { getDirname, path } from '@vuepress/utils'
-// const __dirname = getDirname(import.meta.url)
+import { getDirname, path } from '@vuepress/utils'
+const __dirname = getDirname(import.meta.url)
 
 
 export default defineUserConfig({
   plugins: [
-    // registerComponentsPlugin({
-    //   componentsDir: path.resolve(__dirname, './components'),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components'),
       
-    // }),
+    }),
+    searchPlugin({
+      
+    }),
   ],
   base: "/",
   lang: "zh-CN",
